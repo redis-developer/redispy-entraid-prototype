c4ce704df1a1f490411b3f587ea9b56e24dd00ff
vladvildanov <divinez122@outlook.com>
2024-11-12 12:29:05 +0200

    Removed redundant code

--- redispy-entraid/redisauth/idp.py
+++ redispy-entraid/redisauth/idp.py
@@ -24,37 +24,3 @@ class IdentityProviderConfigInterface(ABC):
     @abstractmethod
     def get_provider(self) -> IdentityProviderInterface:
         pass
-
-
-'''
-A very simple fake identity provider for testing purposes
-'''
-
-
-# class FakeIdentityProvider(IdentityProviderInterface):
-#     SIGN = "secret"
-#
-#     '''
-#     Initialize by authenticating
-#     '''
-#
-#     def __init__(self, user, password):
-#         self.creds = {'user': user, 'password': password}
-#         self.is_authenticated = False
-#
-#         if self.creds['user'] == "testuser" and self.creds['password'] == "password":
-#             self.is_authenticated = True
-#
-#     def request_token(self) -> Token:
-#         if self.is_authenticated:
-#             payload = {
-#                 "user_id": 123,
-#                 "username": "testuser",
-#                 "exp": datetime.utcnow() + timedelta(seconds=10)
-#             }
-#
-#             value = jwt.encode(payload, self.SIGN, "HS256")
-#
-#             return JWToken(value)
-#         else:
-#             raise ErrNotAuthenticated()
