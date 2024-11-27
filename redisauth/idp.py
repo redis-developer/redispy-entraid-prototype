ad4da0fad91975a1ade1e8023a109c37faa930f0
vladvildanov <divinez122@outlook.com>
2024-11-27 11:57:37 +0200

    Removed IDP and cred provider implementations

--- redisauth/idp.py
+++ redisauth/idp.py
@@ -1,9 +1,5 @@
 from abc import ABC, abstractmethod
-
-from msal import ConfidentialClientApplication
-
-from redisauth.err import RequestTokenErr
-from redisauth.token import TokenInterface, JWToken
+from redisauth.token import TokenInterface
 
 '''
 This interface is the facade of an identity provider
@@ -18,17 +14,3 @@ class IdentityProviderInterface(ABC):
     @abstractmethod
     def request_token(self) -> TokenInterface:
         pass
-
-
-class EntraIDIdentityProvider(IdentityProviderInterface):
-    def __init__(self, scopes : list = [], **kwargs):
-        self._app = ConfidentialClientApplication(**kwargs)
-        self._scopes = scopes
-
-    def request_token(self) -> TokenInterface:
-        try:
-            return JWToken(
-                self._app.acquire_token_for_client(self._scopes)["access_token"]
-            )
-        except Exception as e:
-            raise RequestTokenErr(e)
